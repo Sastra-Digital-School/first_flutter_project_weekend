a89147f552db980052c723196e1d24ab835f6f51
Sithy <sithy@Sithys-MacBook-Pro.local>
2025-04-20 21:14:45 +0700

    Update home_screen.dart Date: 20-04-2025

--- lib/app.dart
+++ lib/app.dart
@@ -6,12 +6,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
-      ),
-      home: const HomeScreen(),
-    );
+    return MaterialApp(title: 'Flutter Demo', home: const HomeScreen());
   }
 }
