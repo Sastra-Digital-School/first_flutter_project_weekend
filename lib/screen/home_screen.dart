b1e2f10c4df6fa8790a0697c24749600ccdb6eb0
Sithy <sithy@Sithys-MacBook-Pro.local>
2025-04-20 21:04:44 +0700

    Update home_screen.dart Date: 20-04-2025

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sastra Digital')),
+      appBar: AppBar(title: Text('Sastra Digital School')),
       body: Center(child: Text('Welcome to my world')),
     );
   }
