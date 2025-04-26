93412bafe96a4a2c341a5e92273fd9354b4dafc3
Sithy <sithy@Sithys-MacBook-Pro.local>
2025-04-26 21:30:35 +0700

    update home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,18 +13,10 @@ class HomeScreen extends StatelessWidget {
   }
 
   get _buildBody {
-    return Column(
-      children: [
-        const Text('Home Screen'),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.amber,
-            shape: BoxShape.circle,
-          ),
-          height: 100,
-          width: 100,
-        ),
-      ],
+    return Container(
+      decoration: BoxDecoration(color: Colors.amber, shape: BoxShape.circle),
+      height: 100,
+      width: 100,
     );
   }
 }
